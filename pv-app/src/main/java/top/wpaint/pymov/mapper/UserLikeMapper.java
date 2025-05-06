aa0ddff8a0a895af69d3a4528c9606978da43709
tecwds <tecwds@163.com>
2025-05-06 15:04:56 +0800

    feat(domain): 添加用户喜欢的电影类型实体类和 Mapper 接口
    - 新增 UserLike 类，用于表示用户喜欢的电影类型- 添加 UserLikeMapper 接口，用于处理 UserLike 相关的数据库操作

---
+++ pv-app/src/main/java/top/wpaint/pymov/mapper/UserLikeMapper.java
@@ -0,0 +1,17 @@
+package top.wpaint.pymov.mapper;
+
+import top.wpaint.pymov.domain.UserLike;
+
+/**
+* @author tecwds
+* @description 针对表【pv_user_like】的数据库操作Mapper
+* @createDate 2025-05-06 14:50:24
+* @Entity top.wpaint.pymov.domain.UserLike
+*/
+public interface UserLikeMapper {
+
+}
+
+
+
+
