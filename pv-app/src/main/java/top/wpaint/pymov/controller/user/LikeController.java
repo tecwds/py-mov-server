0e7e1c8ffe61fefc8a8f6c204c3d2623235a59f7
tecwds <tecwds@163.com>
2025-05-13 22:41:57 +0800

    feat(user): 添加用户收藏功能并优化相关操作
    - 在 LikeController 中添加用户收藏添加接口
    - 优化 InsertPvUserTest测试类，增加日志输出和性能测试
    - 在 application.yml 中配置 MyBatis-Plus 日志

--- pv-app/src/main/java/top/wpaint/pymov/controller/user/LikeController.java
+++ pv-app/src/main/java/top/wpaint/pymov/controller/user/LikeController.java
@@ -7,13 +7,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.wpaint.pymov.common.R;
+import top.wpaint.pymov.common.RCode;
 import top.wpaint.pymov.model.dto.user.like.LikeAddDto;
 import top.wpaint.pymov.model.dto.user.like.LikeQueryDto;
+import top.wpaint.pymov.model.entity.UserLike;
 import top.wpaint.pymov.model.vo.user.like.LikeVo;
 import top.wpaint.pymov.service.UserLikeService;
+import top.wpaint.pymov.utils.ThrowUtils;
 import top.wpaint.pymov.utils.ValidUtils;
 
 import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -27,7 +32,7 @@ public class LikeController {
      * 获取用户收藏列表
      *
      * @param likeQueryDto 查询条件
-     * @return
+     * @return 分页数据
      */
     @PostMapping("list")
     public R<Page<LikeVo>> likePage(LikeQueryDto likeQueryDto) {
@@ -35,9 +40,25 @@ public class LikeController {
         return R.ok(userLikeService.getLikePage(likeQueryDto));
     }
 
+    /**
+     * 添加用户收藏
+     *
+     * @param likeAddDto 收藏信息
+     * @return 无
+     */
     @PostMapping("add")
     public R<Void> addLike(LikeAddDto likeAddDto) {
         ValidUtils.requireNotNull(likeAddDto);
+        boolean res = userLikeService.saveBatch(likeAddDto.getLikeGenreIds()
+                .stream()
+                .map(i -> {
+                    UserLike ul = new UserLike();
+                    ul.setUserId(likeAddDto.getUserId());
+                    ul.setGenreId(i);
+                    return ul;
+                })
+                .collect(Collectors.toList()));
+        ThrowUtils.throwIf(!res, RCode.OPERATION_ERROR, "添加失败");
         return R.ok();
     }
 
--- pv-app/src/test/java/top/wpaint/pymov/InsertPvUserTest.java
+++ pv-app/src/test/java/top/wpaint/pymov/InsertPvUserTest.java
@@ -1,6 +1,7 @@
 package top.wpaint.pymov;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import top.wpaint.pymov.model.entity.Genre;
@@ -13,6 +14,7 @@ import top.wpaint.pymov.service.UserService;
 import javax.annotation.Resource;
 import java.util.*;
 
+@Slf4j
 @SpringBootTest
 public class InsertPvUserTest {
 
@@ -28,12 +30,14 @@ public class InsertPvUserTest {
     @Test
     void insert() {
         List<Long> genreIds = new ArrayList<>(genreService.list().stream().map(Genre::getId).toList());
-        List<String> userIds = userService.list(new Page<>(1, 200)).stream().map(User::getUserId).toList();
+        List<String> userIds = userService.list(new Page<>(1, 400)).stream().map(User::getUserId).toList();
 
         List<UserLike> userLikes = new ArrayList<>(2000);
 
         Map<String, List<Long>> userIdAndGenreIdsMap = new HashMap<>();
 
+        int max = 400;
+        int count = 1;
         for (String userId : userIds) {
             Set<Long> genreIdsSet = new HashSet<>();
 
@@ -44,8 +48,16 @@ public class InsertPvUserTest {
             }
 
             userIdAndGenreIdsMap.put(userId, genreIdsSet.stream().toList());
+
+            // 这里打印进度
+            if (count % 10 == 0) {
+                log.info("进度：{} / {}", count, max);
+            }
+
+            count++;
         }
 
+        log.info("开始转换数据");
         userIdAndGenreIdsMap.forEach((userId, gIds) -> {
             gIds.forEach(item -> {
                 UserLike ul = new UserLike();
@@ -54,7 +66,12 @@ public class InsertPvUserTest {
                 userLikes.add(ul);
             });
         });
+        log.info("转换数据结束");
 
-        userLikeService.saveBatch(userLikes);
+        long start = System.currentTimeMillis();
+        userLikeService.saveBatch(userLikes, 500);
+        long end = System.currentTimeMillis();
+
+        log.info("执行保存 SQL 花费了: {}s", (end - start) / 1000);
     }
 }
