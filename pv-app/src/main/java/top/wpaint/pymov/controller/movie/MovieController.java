b5d7297aafedee0a3d57947f7494c40d88d5dbeb
tecwds <tecwds@163.com>
2025-05-14 01:48:56 +0800

    refactor(movie): 修改电影分页查询接口为 POST 请求
    -将 MovieController 中的 getPage 方法从 GET 请求改为 POST 请求
    - 更新了相关的请求映射注解- 这个改动可能是为了处理更复杂的查询参数或提高安全性

--- pv-app/src/main/java/top/wpaint/pymov/controller/movie/MovieController.java
+++ pv-app/src/main/java/top/wpaint/pymov/controller/movie/MovieController.java
@@ -3,10 +3,7 @@ package top.wpaint.pymov.controller.movie;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import top.wpaint.pymov.common.R;
 import top.wpaint.pymov.model.dto.movie.MovieQueryDto;
 import top.wpaint.pymov.model.entity.Movie;
@@ -26,7 +23,7 @@ public class MovieController {
      * @param dto 分页查询条件
      * @return 分页数据
      */
-    @GetMapping("page")
+    @PostMapping("page")
     public R<Page<Movie>> getPage(@RequestBody MovieQueryDto dto) {
         ValidUtils.requireNotNull(dto);
         return R.ok(movieService.getPage(dto));
